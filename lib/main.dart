6e4ab8aa8f1ae865bc3b1902dc228e33325ee168
SD <gumsiz@yandex.com>
2022-08-21 12:32:05 +0200

    added update method to refresh UI

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,25 @@ void main() {
 }
 
 class BackgroundComponent extends Component with HasGameRef {
+  var offsetX = 0.0;
+  var offsetY = 0.0;
   @override
   void render(Canvas canvas) {
-    canvas.rotate(0.7);
+    // canvas.rotate(0.7);
     canvas.drawRect(
       Rect.fromLTWH(0, 0, gameRef.size.x, gameRef.size.y),
       Paint()..color = Colors.white,
     );
     canvas.drawCircle(
-      Offset(gameRef.size.x / 2, gameRef.size.y / 2),
+      Offset(offsetX, offsetY),
       100,
       Paint()..color = Colors.red,
     );
+  }
+
+  @override
+  void update(double t) {
+    offsetX += 2;
+    offsetY += 1;
   }
 }
